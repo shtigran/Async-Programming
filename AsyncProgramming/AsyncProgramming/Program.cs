918a00ad4aa51d945eaca3a5cce595ebd263fdbc
shtigran <shakhbekyantigran@gmail.com>
2017-02-22 15:39:13 +0400

    BySystemThreadingInfo

--- AsyncProgramming/AsyncProgramming/Program.cs
+++ AsyncProgramming/AsyncProgramming/Program.cs
@@ -16,6 +16,7 @@ namespace AsyncProgramming
     {
       // Multithreaded, Parallel, and Async Programming
 
+      # region ByDelegates
       //Invoking a Method Asynchronously by delegate
 
       Console.WriteLine("***** Async Delegate Invocation *****");
@@ -31,7 +32,31 @@ namespace AsyncProgramming
       // method when ready.
       int answer = b.EndInvoke(iftAR);
       Console.WriteLine("10 + 10 is {0}.", answer);
+      #endregion
 
+      #region BySystemThreadingInfo
+    
+        // Be sure to import the System.Threading namespace.
+        Console.WriteLine("***** Primary Thread stats *****\n");
+        // Obtain and name the current thread.
+        Thread primaryThread = Thread.CurrentThread;
+        primaryThread.Name = "ThePrimaryThread";
+        // Show details of hosting AppDomain/Context.
+        Console.WriteLine("Name of current AppDomain: {0}",Thread.GetDomain().FriendlyName);
+        Console.WriteLine("ID of current Context: {0}",Thread.CurrentContext.ContextID);
+        // Print out some stats about this thread.
+        Console.WriteLine("Thread Name: {0}", primaryThread.Name);
+        Console.WriteLine("Has thread started?: {0}",primaryThread.IsAlive);
+        Console.WriteLine("Priority Level: {0}", primaryThread.Priority);
+        Console.WriteLine("Thread State: {0}",primaryThread.ThreadState);
+
+
+      #endregion
+
+      #region ManuallyCreatingSecondThread
+
+
+      #endregion
       Console.ReadKey();
     }
 
